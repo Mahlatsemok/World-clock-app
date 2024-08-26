d35ba2c3087b8d0c68b1a4e0f18f95fad91359fc
Mahlatsemok <mahlatse.mokhabela@gmail.com>
2024-08-26 16:59:48 +0200

    Added the current location

--- src/index.js
+++ src/index.js
@@ -32,6 +32,8 @@ function time() {
 
 function city(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") 
+    cityTimeZone = moment.tz.guess();
     let cityName = cityTimeZone.replace("_", "").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
